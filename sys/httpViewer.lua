b52780134a8150fc5d7de60d8e774b41113afecd
Timoh5709 <153010705+Timoh5709@users.noreply.github.com>
2025-08-01 13:52:12 +0200

    httpception

--- sys/httpViewer.lua
+++ sys/httpViewer.lua
@@ -1,6 +1,6 @@
 local httpViewer = {}
 
-function httpViewer.readUrl(url)
+local function httpBrain(url)
     local request = http.get(url)
     if request == nil then
         term.setTextColor(colors.red)
@@ -8,12 +8,16 @@ function httpViewer.readUrl(url)
         sleep(0.1)
         term.setTextColor(colors.white)
     else
-        print(request.readAll())
+        return request.readAll()
     end
 end
 
+function httpViewer.readUrl(url)
+    print(httpBrain(url))
+end
+
 function httpViewer.getLines(url)
-    local text = httpViewer.readUrl(url)
+    local text = httpBrain(url)
     local lignes = {}
     for ligne in text:gmatch("([^\n]*)\n?") do
         table.insert(lignes, ligne)
