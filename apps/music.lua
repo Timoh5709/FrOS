e51fab5be8a7befd8426ca090cdeec8e9c407b64
Timoh5709 <153010705+Timoh5709@users.noreply.github.com>
2025-08-01 23:37:40 +0200

    update apps/music.lua

---
+++ apps/music.lua
@@ -0,0 +1,37 @@
+local speaker = peripheral.find("speaker")
+local dfpwm = require("cc.audio.dfpwm")
+local decoder = dfpwm.make_decoder()
+local statusBar = require("/sys/statusBar")
+
+local function player(filename)
+  for chunk in io.lines(filename, 16 * 1024) do
+    local buffer = decoder(chunk)
+    
+    while not speaker.playAudio(buffer) do
+      local eventData = {os.pullEvent()}
+      local event = eventData[1]
+
+      if event == "speaker_audio_empty" then
+      elseif event == "key" then
+        speaker.stop()
+      end
+    end
+  end 
+end
+
+local function main()
+  dossier = "music.lua"
+  write(dossier .. "♪ ")
+  statusBar.draw(dossier)
+  local input = read()
+  
+  if fs.exists(input) then
+    print("Joue actuellement : " input)
+  end
+end
+
+if speaker ~= nil then
+  main()
+else
+  print("Erreur : Aucun haut-parleur detecte.")
+end
